99e0d0fcd85f5f22cd227bd09cff939534da01ae
imcoder236 <64865733+imcoder236@users.noreply.github.com>
2020-05-06 21:39:35 +0530

    ok

--- face_app/views.py
+++ face_app/views.py
@@ -33,7 +33,6 @@ import string
 
 def get_random_alphaNumeric_string(stringLength=8):
     lettersAndDigits = string.ascii_letters + string.digits
-    print("done")
     return ''.join((random.choice(lettersAndDigits) for i in range(stringLength)))
 
 def email(request):
